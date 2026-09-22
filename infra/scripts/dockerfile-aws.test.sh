06393f8284ae63d9c1444f34de6a7e93fdda8903
KshitijDani <kdani500@gmail.com>
2026-09-21 18:30:25 -0700

    fix(infra): remove workspace build tools from runtime

--- infra/scripts/dockerfile-aws.test.sh
+++ infra/scripts/dockerfile-aws.test.sh
@@ -9,3 +9,4 @@ grep -Fq "https://truststore.pki.rds.amazonaws.com/us-west-2/us-west-2-bundle.pe
 grep -Fq "openssl crl2pkcs7 -nocrl -certfile $bundle" "$dockerfile"
 grep -Fq "NODE_EXTRA_CA_CERTS=$bundle" "$dockerfile"
 grep -Fq 'npm ci --omit=dev --workspace=llteacher-web --workspace=@llteacher/ui --include-workspace-root=false' "$dockerfile"
+grep -Fq 'rm -rf node_modules/typescript node_modules/vite' "$dockerfile"
